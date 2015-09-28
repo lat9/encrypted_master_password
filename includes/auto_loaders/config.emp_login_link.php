09f20fd846af879b846f427e701f79aa13234c2f
lat9 <lat9@vinosdefrutastropicales.com>
2015-09-28 08:25:29 -0400

    #8: Enable EMP admin password change
    Update to enable a logged-in EMP admin to change the customer's
    password.

--- includes/auto_loaders/config.emp_login_link.php
+++ includes/auto_loaders/config.emp_login_link.php
@@ -2,7 +2,7 @@
 // -----
 // Part of the Encrypted Master Password plugin, provided by lat9@vinosdefrutastropicales.com
 //
-// Copyright (C) 2013-2014, Vinos de Frutas Tropicales
+// Copyright (C) 2013-2015, Vinos de Frutas Tropicales
 //
 // @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 //
@@ -13,8 +13,8 @@ if (!defined('IS_ADMIN_FLAG')) {
 /*
 ** Point 200 is after all other initialization is complete.
 */
-  $autoLoadConfig[200][] = array('autoType'=>'class',
-                                 'loadFile'=>'observers/class.emp_order_observer.php');
-  $autoLoadConfig[200][] = array('autoType'=>'classInstantiate',
-                                 'className'=>'emp_order_observer',
-                                 'objectName'=>'emp_order_observer');
+$autoLoadConfig[200][] = array ('autoType'   => 'class',
+                                'loadFile'   => 'observers/class.emp_order_observer.php');
+$autoLoadConfig[200][] = array ('autoType'   => 'classInstantiate',
+                                'className'  => 'emp_order_observer',
+                                'objectName' => 'emp_order_observer');
