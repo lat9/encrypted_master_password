66bb481b3ea094e3e23cf9a6ba10b4513213d4e7
lat9 <lat9@vinosdefrutastropicales.com>
2016-02-03 12:20:16 -0500

    #11: Add Dutch language translations (need readme)
    Add the Dutch language translations.

---
+++ YOUR_ADMIN/includes/languages/dutch/extra_definitions/emp_button_definitions.php
@@ -0,0 +1,7 @@
+<?php
+// -----
+// Part of the Encrypted Master Password plugin, provided by lat9@vinosdefrutastropicales.com
+//
+// @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+//
+define('EMP_BUTTON_PLACEORDER_ALT', 'Plaats een bestelling voor deze klant');
