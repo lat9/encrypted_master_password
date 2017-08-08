e9a2e357ba85d7f856a7730b15a1acc0068cc195
lat9 <lat9@vinosdefrutastropicales.com>
2017-08-08 14:01:25 -0400

    #22: Drop support for ZC versions < 1.5.5
    Removes the core-file changes required; the 1.5.5 "core" now includes
    the required notifications.

--- includes/init_includes/init_emp_login_link.php
+++
@@ -1,9 +0,0 @@
-<?php
-// -----
-// Part of the Encrypted Master Password plugin, provided by lat9@vinosdefrutastropicales.com
-//
-// Copyright (C) 2013-2014, Vinos de Frutas Tropicales.
-//
-// @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
-//
-// ----> No longer used as of v2.0.0.  This file is safe to delete <-----
